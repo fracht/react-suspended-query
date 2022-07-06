67b7fc7e27cee9517c53da621a2c560a5b765704
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-06 15:34:33 +0300

    Initialized example

---
+++ example/src/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App';
+
+const container = document.querySelector('#root');
+
+const root = createRoot(container as HTMLElement);
+
+root.render(<App />);
