4640c199dfac14517b755f2e825481d9b542ed60
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-08 12:15:40 +0300

    Fixed stringifyKey test

--- test/utils/stringifyKey.test.ts
+++ test/utils/stringifyKey.test.ts
@@ -2,10 +2,14 @@ import { stringifyKey } from '../../src/utils/stringifyKey';
 
 describe('Stringify key', () => {
     it('should stringify string', () => {
-        console.log(stringifyKey('asdf'));
+        expect(stringifyKey('asdf')).toBe(stringifyKey('asdf'));
+        expect(typeof stringifyKey('asdf')).toBe('string');
     });
 
     it('should stringify object', () => {
-        console.log(stringifyKey({ key: 'api/url', payload: { value: 42 } }));
+        expect(stringifyKey({ key: 'api/url', payload: { value: 42 } })).toBe(
+            stringifyKey({ key: 'api/url', payload: { value: 42 } }),
+        );
+        expect(typeof stringifyKey({ key: 'api/url', payload: { value: 42 } })).toBe('string');
     });
 });
