37b15e7b3e2950bb0e5c7b344b6bced46427fdd6
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-11 15:49:05 +0300

    Fixed Fetcher type

--- src/types/Fetcher.ts
+++ src/types/Fetcher.ts
@@ -2,6 +2,4 @@ import { Key } from './Key';
 
 export type Fetcher<TData, TKey extends Key> = TKey extends [...infer Arguments]
     ? (...arguments_: Arguments) => Promise<TData>
-    : TKey extends () => [...infer Arguments]
-    ? (...arguments_: Arguments) => Promise<TData>
     : (argument: TKey) => Promise<TData>;
